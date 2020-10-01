25688ac2e1c7a4d9c60c475ae6354f3d06bceb28
erickm320 <erickm320@icloud.com>
2020-10-01 12:36:31 -0600

    Finish

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Switch, Route} from 'react=router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Classlist from './components/ClassList/ClassList';
+
+export default (
+    <Switch>
+        <Route component = {Home} exact path = '/'/>
+        <Route component = {About} exact path = '/about'/>
+        <Route component ={Classlist} exact path = '/classlist/:class'/>
+        <Route component = {Student} path='/student/:id'/>
+    </Switch>
+);
