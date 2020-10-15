36bbad1a7e3f6b9516888806240c39ab63a3111e
erickm320 <erickm320@icloud.com>
2020-10-15 14:58:43 -0600

    Finish

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom'
 
 export default class ClassList extends Component {
   constructor() {
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {Switch, Route} from 'react=router-dom';
+import {Switch, Route} from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Classlist from './components/ClassList/ClassList';
+import Student from './components/Student/Student';
 
 export default (
     <Switch>
